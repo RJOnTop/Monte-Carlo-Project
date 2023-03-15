4df8036d52ddc7f5ca0d8fe59582a860c28e7e41
RJOnTop <127918203+RJOnTop@users.noreply.github.com>
2023-03-14 21:27:17 -0700

    How is this for Infinite Deck?
    Tried to keep your guys variable names the same so its easier to follow. Not sure if this is how your planning to do this policy but feel free to edit or rewrite

--- Infinite Deck.py
+++ Infinite Deck.py
@@ -1,1 +1,37 @@
+import random
+
+#        2, 3, 4, 5, 6, 7, 8, 9, 10, J,  Q,  K,  A
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+class Infinite_Deck:
+    def __init__(self):
+        self.deck = cards.copy()
+
+    def draw_card(self):
+        
+        if len(self.deck) == 0:     # Once the deck is empty this re-shuffles a brand new one
+            self.deck = cards.copy()
+
+        
+        card = random.choice(self.deck)     # Random card is taken
+
+        
+        self.deck.remove(card)    # Proceeds to remove that card
+
+        return card
+
+
+deck = Infinite_Deck()    # Assigns class to a variable
+
+# Takes a range of cards and prints
+for i in range(20):
+    card = deck.draw_card()
+    print("Card Taken:", card)
+
+# This is an infinite version that continuously prints just put the range above in comments 
+'''
+while True:
+    card = deck.draw_card()
+    print("Card taken:", card)
+'''
 
